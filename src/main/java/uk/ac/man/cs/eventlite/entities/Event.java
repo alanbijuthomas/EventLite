fbaf31dc485c069439ec3cb32bfac87892ae8ed9
Alan Biju Thomas <alan.thomas@student.manchester.ac.uk>
2020-02-29 16:51:10 +0000

    Adds description to event and introduces event attribute constraints

--- src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Event.java
@@ -5,6 +5,8 @@ import java.time.LocalTime;
 
 import javax.persistence.*;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -20,15 +22,22 @@ public class Event {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@NotEmpty(message = "The event must have a date.")
 	private LocalDate date;
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	@DateTimeFormat(pattern = "HH:mm")
 	private LocalTime time;
-
+	
+	@NotEmpty(message = "The event must have a name.")
+	@Size(max = 256, message = "The event's name must have 256 characters or less.")
 	private String name;
 	
+	@Size(max = 500, message = "The event's description must have 500 characters or less.")
+	private String description;
+	
 	@ManyToOne
+	@NotEmpty(message = "The event must have a venue.")
 	private Venue venue;
 
 	public Event() {
@@ -64,6 +73,14 @@ public class Event {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Venue getVenue() {
