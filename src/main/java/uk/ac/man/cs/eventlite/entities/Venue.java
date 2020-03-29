ddd6549393d8707514763b7b76dc4d5520dae7be
Wolodymyr <wolodyak@hotmail.co.uk>
2020-03-29 22:01:39 +0100

    add entity constraints to venue class

--- src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
@@ -4,6 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "venue")
@@ -13,10 +18,22 @@ public class Venue {
 	@GeneratedValue
 	private long id;
 
+	@NotNull(message = "The venue must have a name.")
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@Size(max = 256, message = "The event's description must have 256 characters or less.")
 	private String name;
-	private String address;
-	private String postcode;
 	
+	@NotNull(message = "The venue must have an address.")
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@Size(max = 300, message = "The venue's roadname must have 300 characters or less.")
+	private String address;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull(message = "The venue must have a postcode.")
+	private String postcode;
+
+	@NotNull
+	@Min(0)
 	private int capacity;
 
 	public Venue() {
