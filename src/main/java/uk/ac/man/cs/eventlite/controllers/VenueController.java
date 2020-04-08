151e278d166256e60ccfd88f1cdf2013a59b02ff
Alan Biju Thomas <alan.thomas@student.manchester.ac.uk>
2020-04-08 18:22:35 +0800

    Implements update venue functionality

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenueController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenueController.java
@@ -7,13 +7,17 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.man.cs.eventlite.dao.VenueService;
+import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
 
 @Controller @RequestMapping(value = "/venues", produces = {MediaType.TEXT_HTML_VALUE}) 
@@ -50,5 +54,24 @@ public class VenueController {
         return "redirect:/events";
     }
 	 
+    
+    @GetMapping("/update/{id}")
+	public String updateVenue(@PathVariable("id") long id, Model model)  {
+		model.addAttribute("venue", venueService.findOne(id));
+		return "venues/update-venue";
+	}
+	
+	@PostMapping("/updated/{id}")
+	public String venueUpdated(@PathVariable("id") long id, Venue venue, 
+			  BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			venue.setId(id);
+			model.addAttribute("venue", venueService.findOne(id));
+	        return "venues/update-venue";
+	    }
+		venueService.save(venue);
+        model.addAttribute("venueList", venueService.findAll());
+		return "redirect:/venues";
+	}
 
 }
--- src/main/java/uk/ac/man/cs/eventlite/dao/VenueService.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/VenueService.java
@@ -2,11 +2,14 @@ package uk.ac.man.cs.eventlite.dao;
 
 import uk.ac.man.cs.eventlite.entities.Venue;
 
-public interface VenueService {
+public interface VenueService extends VenueRepository {
 
 	public long count();
 
 	public Iterable<Venue> findAll();
 	
 	public <V extends Venue> V save(V venue);
+	
+	public Venue findOne(long id);
+	
 }
