43b872d1a860bba3de5f96e3e8ae9b130820b27b
Zhaoyu Zhang <zhaoyu.zhang-4@student.manchester.ac.uk>
2020-05-02 22:51:57 +0800

    Added one more test for delete venues with events
    now test getJsonRoot() can pass

--- src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -43,47 +44,6 @@ import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
 import uk.ac.man.cs.eventlite.entities.Venue;
-
-
-//
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.never;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-//import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-//import javax.servlet.Filter;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.annotation.DirtiesContext.ClassMode;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//
-//import uk.ac.man.cs.eventlite.EventLite;
-//import uk.ac.man.cs.eventlite.dao.EventService;
-//import uk.ac.man.cs.eventlite.dao.VenueService;
-//import uk.ac.man.cs.eventlite.entities.Event;
-//import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
@@ -194,5 +154,23 @@ public class VenuesControllerTest {
 					.andExpect(status().isFound());
 		
 		verify(venueService, never()).deleteById(id);
+	}
+	
+	@Test
+	@WithMockUser(roles= "ADMINISTRATOR")
+	public void deleteVenueWithEvents() throws Exception {
+		long id = 1234;
+		Venue venue = mock(Venue.class);
+		Event event = mock(Event.class);
+		venue.addEvent(event);
+		
+		when(venueService.findOne(id)).thenReturn(venue);
+				
+		mvc.perform(delete("/venues/{id}", id).accept(MediaType.TEXT_HTML).with(csrf()))
+					.andExpect(status().isFound())
+					.andExpect(view().name("redirect:/venues"))
+					.andExpect(handler().methodName("deleteVenue"));
+		
+		verify(venueService).deleteById(id);
 	}
 }
